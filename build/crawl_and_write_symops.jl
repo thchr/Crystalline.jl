ab3c0dad3c3808048ea7fbf6525d3f749d691253
tchr <tchr@mit.edu>
2019-07-23 17:31:47 -0400

    straggling changes to crawling of bilbao's symmetry operations

--- build/crawl_and_write_symops.jl
+++ build/crawl_and_write_symops.jl
@@ -1,12 +1,12 @@
 using JSON2, SGOps
 
-#= Small convenience script to crawl and subsequently write all the short-
-   hand forms of the symmetry operations of the 230 three-dimensional
-   space-groups. Enables us to just read the symmetry data from the hard-
-   disk rather than constantly querying the Bilbao server =#
+#= Small convenience script to crawl and subsequently write all the xyzt
+   forms of the symmetry operations of the 230 three-dimensional space-
+   groups. Enables us to just read the symmetry data from the hard-disk
+   rather than constantly querying the Bilbao server =#
 for i = 1:230
-    symops_str = SGOps.crawl_symops_shorthand(i)
-    filename = (@__DIR__)*"/../json/symops/3d/sg"*string(i)*".json"
+    symops_str = SGOps.crawl_symops_xyzt(i)
+    filename = (@__DIR__)*"/../data/symops/3d/sg"*string(i)*".json"
     open(filename; write=true, create=true, truncate=true) do io
         JSON2.write(io, symops_str)
     end 
