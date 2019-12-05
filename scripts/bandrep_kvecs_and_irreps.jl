2d94e88497a73be4591b6d8d78059bef5c1f7bb1
Thomas Christensen <tchr@mit.edu>
2019-12-05 18:08:21 -0500

    Better tooling in scripts/bandrep_kvecs_and_irreps.jl to determine precisely which special LGIrreps we are msising in ISOTROPY that are needed for bandreps.

--- scripts/bandrep_kvecs_and_irreps.jl
+++ scripts/bandrep_kvecs_and_irreps.jl
@@ -4,35 +4,44 @@ if !isdefined(Main, :LGIRS)
     LGIRS = get_all_lgirreps(3)  # loaded from our saved .jld2 files
 end
 
-allpaths = false
-spinful = false
+allpaths = true
+spinful  = false
+brtype   = "Elementary TR"
 showmissing = true
-@testset begin "Copy of test in test/bandrep.jl"
-# --- test physically irreducible irreps/co-reps (assuming time-reversal symmetry) ---
+# --- test physically irreducible irreps/co-reps ---
+@testset "Are all the k-vectors from bandreps in ISOTROPY?" begin
 incomplete_sgs = Vector{Int64}()
+incomplete_lgirs = Dict{Int64,Array{String}}()
 for (sgnum, lgirsvec) in enumerate(LGIRS)
-    BRS = bandreps(sgnum, allpaths, spinful, "Elementary TR")
+    BRS = bandreps(sgnum, allpaths, spinful, brtype)
     irlabs_BRS = BRS.irreplabs
     klabs_BRS = BRS.klabs
 
     irlabs_ISO = Vector{String}()
-    realirlabs_ISO = Vector{String}()
     klabs_ISO = Vector{String}(undef, length(lgirsvec))
     for (kidx, lgirs) in enumerate(lgirsvec)
-        append!(irlabs_ISO,     [label(lgir) for lgir in lgirs])
-        append!(realirlabs_ISO, realify(lgirs)[2])
+        if brtype == "Elementary"
+            append!(irlabs_ISO, [label(lgir) for lgir in lgirs])
+        elseif brtype == "Elementary TR"
+            append!(irlabs_ISO, realify(lgirs)[2])
+        end
         klabs_ISO[kidx] = klabel(first(lgirs))
     end
     irlabs_ISO = SGOps.formatirreplabel.(irlabs_ISO)
-    realirlabs_ISO = SGOps.formatirreplabel.(realirlabs_ISO)
+
 
     for (iridx_BRS, irlab_BRS) in enumerate(irlabs_BRS)
         klab_BRS = klabel(irlab_BRS)
-        if irlab_BRS ∉ realirlabs_ISO
+        if irlab_BRS ∉ irlabs_ISO
             if showmissing
                 @info "Cannot find real irrep $(irlab_BRS) in ISOTROPY dataset (sgnum = $sgnum)"
             end
             sgnum ∉ incomplete_sgs && push!(incomplete_sgs, sgnum)
+            if !haskey(incomplete_lgirs, sgnum)
+                incomplete_lgirs[sgnum] = [irlab_BRS]
+            else
+                push!(incomplete_lgirs[sgnum], irlab_BRS)
+            end
             @test_broken false
             
         else
@@ -44,8 +53,11 @@ for (sgnum, lgirsvec) in enumerate(LGIRS)
         end
     end
 end
+
 if !isempty(incomplete_sgs)
     print(stdout, "The following space groups have incomplete little group (TR-invariant) irreps in ISOTROPY:\n\t")
     join(stdout, incomplete_sgs, ", "); println()
+    sorted_incomplete_lgirs = sort(keys(incomplete_lgirs) .=> values(incomplete_lgirs), by=x->getfield(x,1))
+    display(sorted_incomplete_lgirs)
 end
 end
