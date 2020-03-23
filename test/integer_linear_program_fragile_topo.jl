65e7d3f62fbf89eb74f7edd7177d04d95f334d66
Thomas Christensen <tchr@mit.edu>
2020-03-23 14:58:29 -0400

    Scripts to explore integer linear programming and the compatibility relation bases/band representations

---
+++ test/integer_linear_program_fragile_topo.jl
@@ -0,0 +1,33 @@
+using JuMP, GLPK, SGOps
+
+# see documentation for JuMP at http://www.juliaopt.org/JuMP.jl/v0.21.1
+sgnum = 230
+BRS = bandreps(sgnum)       # 
+N = length(BRS)             # number of BandReps
+A = matrix(BRS)             # "atomic" basis
+B = SGOps.wyckbasis(BRS)[1] # compat. rel. basis
+
+# pick a symmetry vector out of a hat
+c₀ = zeros(Int64, N)
+c₀[1] = 1; c₀[3] = 2; c₀[5] = 4; c₀[end] = 1
+#c₀ = [0,-1,0,-2,0,-4,7,7]; # for sg #3 this is an equivalent symmetry indicator to c₀=[1,0,3,0,4,0,0,0]
+n = A'*c₀
+
+println("c_orig  = ", c₀)
+
+# feasibility problem with positive-integer variables, subject to the condition Aᵀc = n
+m = Model(GLPK.Optimizer)
+@variable(m, c[1:N] >= 0, Int)
+@constraint(m, A'*c .== n)
+
+# solve the model and print solution
+optimize!(m)
+println("c_optim = [", join(Int.(value.(c)), ", "), "]")
+
+# try with Nemo.jl instead
+using Nemo
+A′ = Nemo.matrix(ZZ, size(A')..., A')
+n′ = MatrixSpace(ZZ,size(n,1),1)(n)
+_, x′ = cansolve(A′, n′)
+
+println("c_nemo  = ", Int.(vec(Matrix(x′)))) # in general will not get a nice factorization from this
