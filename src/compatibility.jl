45393d518fa62812da58b23c872c1f5147046184
tchr <tchr@mit.edu>
2020-03-23 15:14:33 -0400

    Early work to try calculating the compatibility allowed band bases (WIP)

--- src/compatibility.jl
+++ src/compatibility.jl
@@ -212,9 +212,157 @@ function compatibility_matrix(BRS::BandRepSet)
             if compat_bool
                 nᴳᴴᵢⱼ = subduction_count(Dᴳᵢ, Dᴴⱼ, αβγ′)
                 if !iszero(nᴳᴴᵢⱼ)
-                    # TODO (more complicated than I thought: have to match across different special lgirreps)
+                    # TODO: more complicated than I thought: have to match across different special lgirreps
                 end 
             end
         end
     end
+end
+
+"""
+    compatibility_matrix(lgirsvec)
+
+Compute all compatibility relations between lines and connectible points in k-space, each
+forming a row in a matrix. Also include all filling constraints (i.e. enforcing that a band
+must have the same number of states at every k-point). This is essentially the approach 
+described in Song, Zhang, & Fang PRX 8, 031069 (2018)
+
+TODO: Unfortunately, this doesn't appear to work generally yet, as can be verified by 
+      comparing with the basis obtained from wyckbasis(bandrep(...)), e.g. as in 
+    
+    for sgnum in 1:230
+        v = (size(SGOps.wyckbasis(bandreps(sgnum))[1],1), 
+             size(SGOps.compatibility_basis(get_lgirreps(sgnum)),2))
+        println(sgnum, ": ", v[1] == v[2], " ", v)
+    end
+"""
+function compatibility_matrix(lgirsvec)
+    kvs    = kvec.(first.(lgirsvec))
+    klabs  = klabel.(first.(lgirsvec))
+    irlabs = [label(lgirs) for lgirvec in lgirsvec for lgirs in lgirvec]
+    #println(irlabs)
+    #pop!(irlabs) # remove Γ point (always last)
+    #println(irlabs)
+    Nk     = length(kvs)
+    Nirr   = length(irlabs)
+    buffer = zeros(Int, Nirr) # preallocated constraint relation buffer
+    Crels  = Vector{Vector{Int}}()
+    for (kidxᴴ, Dᴴᵢs) in enumerate(lgirsvec)     # subgroup 𝐤 (lower symmmetry)
+        kvᴴ = kvs[kidxᴴ]
+        nᵅᵝᵞᴴ = nfreeparams(kvᴴ)
+        # we let kvᴴ denote a "lower symmetry" k-line, -plane, or -volume, and want
+        # to initially determine how many "higher symmetry" k-point (kvᴳ) it connects to 
+        nᵅᵝᵞᴴ ≠ 0 || continue
+        for (kidxᴳ, Dᴳᵢs) in enumerate(lgirsvec) # supergroup 𝐤 (higher symmmetry)
+            kidxᴳ == kidxᴴ && continue
+            kvᴳ = kvs[kidxᴳ]
+            nᵅᵝᵞᴳ = nfreeparams(kvᴳ) 
+            nᵅᵝᵞᴳ == 0 || continue # we restrict kvᴳ to high symmetry _points_
+            # specifically, not all planes (of little group H) that can be connected to a 
+            # line (of group G) actually have the necessary relationship H<G
+            
+            compat_bool, αβγᴴ = is_compatible_kvec(kvᴳ, kvᴴ)
+            compat_bool || continue # kvᴴ and kvᴳ must be compatible
+            
+            subducts = zeros(Int, length(Dᴳᵢs)) # subduction mappings from Dᴳʲ to Dᴴ
+            Dᴳᵢidxs = (:)(findfirst(==(label(first(Dᴳᵢs))), irlabs),
+                          findfirst(==(label(last(Dᴳᵢs))),  irlabs))
+            for Dᴴᵢ in Dᴴᵢs                                     # subgroup irrep
+                Dᴴᵢidx = findfirst(==(label(Dᴴᵢ)), irlabs)
+                for (j, Dᴳᵢ) in enumerate(Dᴳᵢs)                 # supergroup irrep
+                    # how many times does Dᴳ subduce into Dᴴ
+                    subducts[j] = subduction_count(Dᴳᵢ, Dᴴᵢ, αβγᴴ)
+                end
+                # `subducts` now give the a different ways that Dᴳs can subduce into Dᴴ; to 
+                # get a conserved quantity, we look at the "sum" all these "options". we
+                # have to take special care to allow cases where subduction counts differ:
+                # As an example, consider the fictious scenario where irreps at Γ subduce
+                # into a particular line irrep Λ₁ 
+                #   Γ₁ → Λ₁,   Γ₂ → 2Λ₁,   Γ₃ → 3Λ₁   ⇐ (equiv. to `subducts = [1 2 3]`)
+                # In this case, the overall "conservation law" is 
+                #   6n(Γ₁) + 3n(Γ₂) + 2n(Γ₃) = 6n(Λ₁) 
+                # with n(Dᵏ) denoting the number of times irrep Dᵏ occurs at k. We find the 
+                # appropriate prefactors by taking the least common multiple of `subducts`
+                max_subduct = lcm(filter(!iszero, subducts))
+                for (j,s) in enumerate(subducts)
+                    buffer[Dᴳᵢidxs[j]] = (s == 0 ? 0 : Int(max_subduct/s))
+                end
+                buffer[Dᴴᵢidx]  = -max_subduct
+
+                push!(Crels, copy(buffer))  # write new compatibility relation
+                buffer .= 0                 # reset buffer before next irrep from Dᴴᵢs
+            end
+        end
+    end
+    
+    # TODO: Don't really need to add this equality constraint if the points are connectible
+    for (kidxᴳ, Dᴳᵢs) in enumerate(lgirsvec)         # supergroup (higher symmmetry)
+        # for all-kpoints that are not connectible, we add the constraints that there must 
+        # be equally many states at each k-point
+        Dᴳᵢidx_min = findfirst(==(label(first(Dᴳᵢs))), irlabs)
+        Dᴳᵢidx_max = findfirst(==(label(last(Dᴳᵢs))), irlabs)
+        dimsᴳᵢ = irdim.(Dᴳᵢs)
+        nstatesᴳ = lcm(dimsᴳᵢ)
+        nirrᴳ = Int64.(nstatesᴳ./dimsᴳᵢ) # same number of states (=nstatesᴳ) across irreps at kᴳ      
+        for kidxᴴ in kidxᴳ+1:Nk # no need to include redunant combinations
+            buffer .= 0 # reset buffer: new constraint 
+            buffer[Dᴳᵢidx_min:Dᴳᵢidx_max] .= nirrᴳ
+
+            #isspecial(kvs[kidxᴴ]) && continue
+
+            Dᴴᵢs = lgirsvec[kidxᴴ]
+            Dᴴᵢidx_min = findfirst(==(label(first(Dᴴᵢs))), irlabs)
+            Dᴴᵢidx_max = findfirst(==(label(last(Dᴴᵢs))), irlabs)
+
+            dimsᴴᵢ = irdim.(Dᴴᵢs)
+            nstatesᴴ = lcm(dimsᴴᵢ)
+            nirrᴴ = Int64.(nstatesᴴ./dimsᴴᵢ) # same number of states (=nstatesᴴ) across irreps at kᴴ
+            buffer[Dᴴᵢidx_min:Dᴴᵢidx_max] .-= nirrᴴ
+
+            if nstatesᴳ ≠ nstatesᴴ # rebalance if there's an unequal number of states in kᴳ and kᴴ, 
+                fᴳᴴ = lcm(nstatesᴴ, nstatesᴳ)
+                buffer[Dᴳᵢidx_min:Dᴳᵢidx_max] .*= fᴳᴴ/nstatesᴳ
+                buffer[Dᴴᵢidx_min:Dᴴᵢidx_max] .*= fᴳᴴ/nstatesᴴ
+            end
+            push!(Crels, copy(buffer))
+        end
+    end
+    #pretty_table(vcat(Crels'...), irlabs)
+
+    return vcat(Crels'...) # the action of C≡vcat(Crels'...) on a valid symmetry vector is a zero vector.
+end
+
+"""
+    compatibility_basis(lgirsvec)
+
+Compute a basis for the space of band structures allowed by compatibility relations and
+"filling" constraints. Return a matrix whose columns give an integer-span of all physically 
+realizable band structures with symmetry content in `lgirsvec` (a vector of vector of 
+`LGIrrep`s, indexed across k-points and distinct irreps). 
+The rows of the basis are the distinct irrep labels, ordered [Γ₁, Γ₂, ..., M₁, ..., Ω].
+
+This basis should span the same space as `wyckbasis(bandreps(num(lgirsvec)))[1]'` (provided
+that `lgirsvec` feature all the necessary k-points). Note that the irrep-content may not be 
+the same in the two, so a comparison should make sure to project out non-shared irreps.
+
+TODO: This doesn't seem to agree with Song, Zhang, & Fang PRX 8, 031069 (2018) Table II,
+      nor with SGOps.wyckbasis(bandreps(10))[1]'. Test for SG 10.
+"""
+function compatibility_basis(lgirsvec)
+    C = compatibility_matrix(lgirsvec)
+
+    # Get nullspace of C with integer coefficients; i.e. find the nullspace in a field of 
+    # integers ℤ. 
+    # Using the Smith Normal Form: For A = SΛT, we can obtain the nullspace of A from the
+    # last n columns of T⁻¹ with n denoting the number of zeros in Λ [i.e. n=nullity(A); 
+    # contrast this with r=rank(A). See e.g. https://core.ac.uk/download/pdf/82343294.pdf 
+    # regarding the Smith normal form and its application to null spaces. See also 
+    # scripts/derive_sg2_bandrep.jl
+    F = SmithNormalForm.smith(C) # Smith Normal Form
+    T⁻¹, Λ = F.Tinv, F.SNF
+    r = sum(!iszero, Λ) # number of nonzeros in Smith normal diagonal matrix = rank(C)
+    zidxs  = r+1:length(Λ)
+    basis = T⁻¹[:, zidxs] # the columns of T⁻¹ are the new basis
+
+    return basis
 end
