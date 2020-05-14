868b83660a8878d25a5a6aacc6a2a956ea423329
tchr <tchr@mit.edu>
2020-05-14 14:42:02 -0400

    Fixes and extensions to `find_representation`

--- src/symeigs2irrep.jl
+++ src/symeigs2irrep.jl
@@ -1,22 +1,33 @@
 """
     find_representation(symvals::AbstractVector{Number}, 
                         lgirs::AbstractVector{<:AbstractIrrep},
-                        αβγ::Union{AbstractVector{<:Real},Nothing}=nothing)
-                                                                            --> Vector{Int}
+                        αβγ::Union{AbstractVector{<:Real},Nothing}=nothing,
+                        assert_return_T::Type{<:Union{Integer, AbstractFloat}}=Int))
+
+                        --> Vector{assert_return_T}
 
 From a vector (or vector of vectors) of symmetry eigenvalues `symvals` sampled along all the
-operations of a group gᵢ, whose irreps are contained in `irs`, return the multiplicities of
-each irrep.
+operations of a group gᵢ, whose irreps are contained in `irs` (evaluated with optional free 
+parameters `αβγ`), return the multiplicities of each irrep.
 
-Effectively, this applies the projection operator P⁽ʲ⁾ of each irrep (j = 1 , ..., Nⁱʳʳ) to
-the symmetry data sᵢ ≡ `symvals`:
+Optionally, the multiciplities' element type can be specified via the `assert_return_T`
+argument (performing checked conversion; returns `nothing` if representation in 
+`assert_return_T` is impossible). This can be useful if one suspects a particular band to 
+transform like a fraction of an irrep (i.e., the specified symmetry data is incomplete).
+
+# Extended help
+Effectively, this applies the projection operator P⁽ʲ⁾ of each irrep's character set
+χ⁽ʲ⁾(gᵢ) (j = 1, ... , Nⁱʳʳ) to the symmetry data sᵢ ≡ `symvals`:
+
     P⁽ʲ⁾  ≡ (dⱼ/|g|) ∑ᵢ χ⁽ʲ⁾(gᵢ)*gᵢ         [characters χ⁽ʲ⁾(gᵢ), irrep dimension dⱼ]
     P⁽ʲ⁾s = (dⱼ/|g|) ∑ᵢ χ⁽ʲ⁾(gᵢ)*sᵢ = nⱼ,   [number of bands that transform like jth irrep]
+
 returning the irrep multiplicities mⱼ ≡ nⱼ/dⱼ.
 """
 function find_representation(symvals::AbstractVector{<:Number}, 
                              lgirs::AbstractVector{<:AbstractIrrep},
-                             αβγ::Union{AbstractVector{<:Real},Nothing}=nothing)
+                             αβγ::Union{AbstractVector{<:Real},Nothing}=nothing,
+                             assert_return_T::Type{<:Union{Integer, AbstractFloat}}=Int)
     ct = CharacterTable(lgirs, αβγ)
     χs = ct.chartable # character table as matrix (irreps-as-columns & operations-as-rows)
 
@@ -43,32 +54,48 @@ function find_representation(symvals::AbstractVector{<:Number},
     # the sense χ†(χ/|g|)χ† = χ†. This could almost certainly be exploited to go from (2b)
     # to (3), but it is not really important.
     #
+    # Unfortunately, (1) doesn't seem to hold if we work with "realified" co-reps (i.e. 
+    # "doubled" complex or pseudoreal irreps). In that case, we do _not_² have χ†χ = |g|I. 
+    # TODO: I'm not sure why this ruins (2) and not (3), but that seems to be the case. 
+    #       As such, we just use (3), even if it is slightly slower.
+    #
     # ¹⁾ We can choose to work with a sum of symmetry eigenvalues of multiple bands - to
     #    obtain the irrep multiplicities of a band multiplet - because (1) is a linear 
     #    one-to-one relation.
+    # ²⁾ Instead, χ†χ is still a diagonal matrix, but the diagonal entries corresponding to
+    #    doubled irreps are 2|g| instead of |g| (from testing).
 
-    inv_g_order = inv(order(first(lgirs)))
-    ms = inv_g_order .* (χs' * symvals) # irrep multiplicities obtained from (2b)
+    #inv_g_order = inv(order(first(lgirs)))                         # [not used; see above]
+    #ms = inv_g_order .* (χs' * symvals) # irrep multiplicities obtained from (2b)
+    
+    ms = χs\symvals # Adrian's approach
 
-    #ms = χs\symvals # Adrian's approach
-
-    # check that imaginary part is numerically zero and that all entries are integer-valued
+    # check that imaginary part is numerically zero and that all entries are representible
+    # in type assert_return_T
     msℝ = real.(ms)
-    msℕ = round.(Int, msℝ)
     @assert isapprox(msℝ, ms,  atol=DEFAULT_ATOL)
-    @assert isapprox(msℕ, msℝ, atol=DEFAULT_ATOL)
+    if assert_return_T <: Integer
+        msT = round.(assert_return_T, msℝ)
+        if isapprox(msT, msℝ, atol=DEFAULT_ATOL)
+            return msT
+        else
+            # if ms cannot be represented by the the requested integer type, we return a
+            # sentinel value of `nothing` to indicate this (could e.g. mean that the input
+            # symmetry data is "incomplete" and needs to be "expanded" (i.e. add # bands))
+            return nothing
+        end
 
-    # TODO: Return something more meaningful than an assertion error if ms is not real or
-    #       integer; could just mean that the symmetry data fed to the method is incomplete
-    #       and needs to be "expanded" (i.e. add bands). E.g., return nothing as a sentinel?
+    elseif assert_return_T <: AbstractFloat
+        return convert.(assert_return_T, msℝ)
+    end
 
-    return msℕ
 end
 function find_representation(multiplet_symvals::AbstractVector{<:AbstractVector{<:Number}}, 
                              lgirs::AbstractVector{<:LGIrrep}, 
-                             αβγ::Union{AbstractVector{<:Real},Nothing}=nothing)
+                             αβγ::Union{AbstractVector{<:Real},Nothing}=nothing,
+                             assert_return_T::Type{<:Union{Integer, AbstractFloat}}=Int)
     # If multiple symmetry values are given, as a vector of vectors, we take their sum over
     # the band-multiplet to determine irrep of the overall multiplet (see discussion in main
     # method)
-    return find_representation(sum.(multiplet_symvals), lgirs, αβγ) 
+    return find_representation(sum.(multiplet_symvals), lgirs, αβγ, assert_return_T) 
 end
