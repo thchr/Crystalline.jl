fc136781261fa2ee4db5794f03dadbd78ba7832f
thchr <tchr@mit.edu>
2024-01-11 17:14:25 +0100

    add a quick docstring to `mspacegroup`

--- src/assembly/groups/mspacegroup.jl
+++ src/assembly/groups/mspacegroup.jl
@@ -1,3 +1,14 @@
+"""
+    mspacegroup(BNS₁, BNS₂)
+    mspacegroup(OG₃)        --> MSpaceGroup{3}
+
+Return the magnetic space group with BNS numbers `(BNS₁, BNS₂)` or the sequential OG number
+`OG₃` (from 1 to 1651).
+
+## Data sources
+The data underlying this function was retrieved from ISOTROPY's compilation of Daniel 
+Litvin's magnetic space group tables (http://www.bk.psu.edu/faculty/litvin/Download.html).
+"""
 function mspacegroup(BNS₁::Integer, BNS₂::Integer, Dᵛ::Val{D}=Val(3)) where D
     D == 3 || throw(DomainError("only 3D magnetic space groups are supported"))
     msgnum = (BNS₁, BNS₂)
