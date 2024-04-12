3248248e004886df0a4f6c914b858192570e4df3
Thomas Christensen <tchr@mit.edu>
2024-04-12 11:41:46 +0200

    more fixes to broken doctest

--- src/symops.jl
+++ src/symops.jl
@@ -508,9 +508,9 @@ The identity operation ``1`` is always included in ``\\{g_i\\}``.
 
 ## Example
 ```jldoctest cosets
-julia> G = pointgroup("6mm")
+julia> G = pointgroup("6mm");
 
-julia> H = pointgroup("3")
+julia> H = pointgroup("3");
 
 julia> Q = cosets(G, H)
 4-element Vector{SymOperation{3}}:
@@ -528,7 +528,7 @@ julia> [compose.(Ref(q), H) for q in Q]
  [m₁₁₀, m₁₀₀, m₀₁₀]
  [m₋₁₁₀, m₁₂₀, m₂₁₀]
 ```
-"""  
+"""
 function cosets(
             G::AbstractVector{T},
             H::AbstractVector{T}
