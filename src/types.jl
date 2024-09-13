4023908dd2721746f2864611c4a87e986ee2e1ba
Thomas Christensen <tchr@mit.edu>
2024-09-13 23:28:27 +0200

    docstring nit

--- src/types.jl
+++ src/types.jl
@@ -811,7 +811,7 @@ A wrapper around a `Vector{T}`, that allows custom printing and dispatch rules o
 
 In Crystalline, it is assumed that all elements of the wrapped vector are associated with
 the _same_ space or point group. Accordingly, if `T` implements [`dim`](@ref) or 
-[`num`](@ref), either must return the same element for each element of the `Collection`
+[`num`](@ref), either must return the same value for each element of the `Collection`
 (and is equal to `dim(::Collection)` and `num(::Collection)`).
 """
 struct Collection{T} <: AbstractVector{T}
