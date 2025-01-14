8bb98d468bd835d51fbad4ea3ad1d3db19d694c2
Thomas Christensen <tchr@mit.edu>
2025-01-14 14:30:42 +0100

    add docs for `SymmetryVector{D}`

--- src/types_symmetryvectors.jl
+++ src/types_symmetryvectors.jl
@@ -5,6 +5,33 @@ abstract type AbstractSymmetryVector{D} <: AbstractVector{Int} end
 # ---------------------------------------------------------------------------------------- #
 # SymmetryVector
 
+"""
+    SymmetryVector{D} <: AbstractSymmetryVector{D}
+
+A symmetry vector in dimension `D`, containing the featured irreps and their multiplicities
+and overall band occupation number.
+
+## Fields
+- `lgirsv :: Vector{Collection{LGIrrep{D}}}` (`const`): a vector of `LGIrrep{D}` collections
+  associated with each high-symmetry **k**-point of a space group.
+- `multsv :: Vector{Vector{Int}}` (`const`): a vector of associated irrep multiplicities.
+  The irrep `lgirsv[i][j]` occurs with multiplicity `multsv[i][j]` in the symmetry vector.
+- `occupation :: Int`: the occupation (number of bands) associated with the symmetry vector.
+
+## Interface
+- The irreps, multiplicities, and band occupation of a `SymmetryVector` can be obtained via
+  `irreps`, `multiplicities`, and `occupation`, respectively.
+- The irrep-labels, **k**-labels, space group number of a `SymmetryVector` are available via
+  `irreplabels`, `klabels`, and `num`, respectively.
+- The `SymmetryVector` can be converted to a "raw" concatenated vector representation via
+  `Vector`.
+
+## Construction
+- From strings: see `parse(::Type{SymmetryVector{D}}, ::AbstractString,
+  ::Vector{Collection{LGIrrep{D}}})`
+- From "raw" concatenated vectors: see [`SymmetryVector(::AbstractVector{<:Integer},
+  ::AbstractVector{<:AbstractString}, :: AbstractDict{String, <:AbstractVector{LGIrrep{D}}})`](@ref)
+"""
 mutable struct SymmetryVector{D} <: AbstractSymmetryVector{D}
     const lgirsv :: Vector{Collection{LGIrrep{D}}}
     const multsv :: Vector{Vector{Int}}
@@ -409,7 +436,7 @@ end
 # CompositeBandRep
 
 """
-CompositeBandRep{D} <: AbstractSymmetryVector{D}
+    CompositeBandRep{D} <: AbstractSymmetryVector{D}
 
 A type representing a linear rational-coefficient combination of `NewBandRep{D}`s. 
 
