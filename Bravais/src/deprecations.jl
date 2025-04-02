3d34d1c3609ad86ac1ece6b088c3a222cee358f9
Thomas Christensen <tchr@mit.edu>
2025-04-02 10:49:55 +0200

    one more deprecation variant to `reciprocalbasis`

--- Bravais/src/deprecations.jl
+++ Bravais/src/deprecations.jl
@@ -1,2 +1,3 @@
+@deprecate reciprocalbasis(Rs::Union{AbstractVector, NTuple}) dualbasis(DirectBasis(Rs))
 @deprecate reciprocalbasis(Rs::DirectBasis) dualbasis(Rs)
 @deprecate directbasis(Rs::ReciprocalBasis) dualbasis(Rs)
